7df6b18dd1ab7d7baab0ccdef178a2c149250a70
apriryan29 <144941770+apriryan29@users.noreply.github.com>
2024-06-02 10:46:18 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+const loginr=document.querySelector('.login')
+const loginlink=document.querySelector('.login-link')
+const registerlink=document.querySelector('.register-link')
+registerlink.addEventListener('click',()=>{loginr.classList.add('active')})
+loginlink.addEventListener('click',()=>{loginr.classList.remove('active')})
